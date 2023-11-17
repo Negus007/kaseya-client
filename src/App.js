691c64b13b569cc1df3bdf72da133710f2f26949
Negus007 <141578316+Negus007@users.noreply.github.com>
2023-11-17 08:08:26 -0500

    testing api calls

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Octokit } from "@octokit/rest";
+import axios from "axios";
+const octokit = new Octokit({
+  auth: "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",
+});
 
 function App() {
+  const [owner, setOwner] = useState("");
+  const [repo, setRepo] = useState("");
+  const [contributors, setContributors] = useState([]);
+  const [avgPulls, setAvgPulls] = useState(0);
+  const [avgCommits, setAvgCommits] = useState([]);
+  const handleOwner = (e) => {
+    setOwner(e.target.value);
+  };
+
+  const handleRepo = (e) => {
+    setRepo(e.target.value);
+  };
+  const getAndListIssues = async () => {
+    const contributions = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/contributors`,
+    );
+    {
+      /*const pulls = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/pulls`,
+    );*/
+    }
+    const pulls = await octokit.pulls.list({
+      owner,
+      repo,
+      state: "all",
+    });
+    {
+      /*const commits = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/commits`,
+    );*/
+    }
+
+    const comments = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/pulls/comments`,
+    );
+    const comments2 = await octokit.repos.listCommits({
+      owner,
+      repo,
+    });
+
+    setAvgPulls(pulls.data.length);
+
+    setContributors(contributions.data);
+  };
+  useEffect(() => {
+    if (contributors) {
+      const getCommits = async () => {
+        const commits = await octokit.repos.listCommits({
+          owner,
+          repo,
+        });
+
+        setAvgCommits(commits.data.length / contributors.length);
+      };
+      getCommits();
+    }
+  }, [contributors]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <label htmlFor="owner">Owner:</label>
+      <input onChange={handleOwner} id="owner" />
+      <label htmlFor="repo">Repo:</label>
+      <input onChange={handleRepo} id="repo" />
+      <button onClick={getAndListIssues}>List Issues</button>
+      <h2>Avg Pull</h2>
+      <p>{avgPulls}</p>
+      <h2>Avg Commits</h2>
+      <p>{avgCommits}</p>
+      <ul>
+        {contributors.map((contributor) => {
+          return (
+            <>
+              <li>
+                <section>
+                  <h2>{contributor.login}</h2>
+                  <img src={contributor.avatar_url} />
+                  <p>{contributor.contributions}</p>
+                </section>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
